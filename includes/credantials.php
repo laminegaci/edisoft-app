70f8ea071695290b3942c7e59421a58d874cefe2
benounnas Oussama <oussamalecool@gmail.com>
2019-03-16 16:22:15 +0100

    Na7it espace

--- includes/credantials.php
+++ includes/credantials.php
@@ -4,7 +4,7 @@ define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 
 if(PHP_OS == 'WINNT'){
-    define("DB_PASS", " ");
+    define("DB_PASS", "");
 }else{
     define("DB_PASS", "LinuxMate2019:D");
 
